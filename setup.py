b9cc31a40adbf238896b766a4bb930109320022a
Matthew Davidson <matthew.davidson@generalfusion.com>
2023-01-20 14:21:54 -0800

    added requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setup(
     name="graphnet",
     version="0.1.0",
     description="A simple graph network comprising of nodes & edges for network analysis",
-    url="https://github.com/shuds13/pyexample",
+    url="https://github.com/generalmattza/graph-network.git",
     author="Matthew Davidson",
     author_email="matthew.davidson@generalfusion.com",
     license="",
     packages=["graphnet"],
     install_requires=[
+        "graphnet",
         "pandas",
         "openpyxl",
     ],
@@ -17,8 +18,5 @@ setup(
         "Development Status :: 1 - Planning",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python :: 3.9",
-    ],
-    scripts=[
-        "tests/test_build_graphnetwork_from_file.py",
     ],
 )
