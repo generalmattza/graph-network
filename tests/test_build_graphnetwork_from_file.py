243bedb4f812dce8e8e91dede7b639086513ede5
Matthew Davidson <matthew.davidson@generalfusion.com>
2023-01-23 13:14:41 -0800

    changed path to fwd slash

--- tests/test_build_graphnetwork_from_file.py
+++ tests/test_build_graphnetwork_from_file.py
@@ -2,6 +2,6 @@ def test_build_graphnetwork_from_file():
 
     from graphnet.network import read_graph_network_from_xls
 
-    filepath = "tests\example_graph_network_input.xlsx"
+    filepath = "tests/example_graph_network_input.xlsx"
     network = read_graph_network_from_xls(filepath)
     print(network.nodes_sorted)
